3ad3eea1d3c8db1c446e482d6346880e60ec314a
Brittany Rife Magalis <brittany.rife@ufl.edu>
2023-01-06 15:41:36 -0500

    removing all old files

--- training_validation_application.R
+++
@@ -1,9 +0,0 @@
-library('dplyr')
-library('tidyverse')
-library('stringr')
-
-#train the model with PART from Weka
-train_model = system(paste('java --add-opens java.base/java.lang=ALL-UNNAMED -Xmx1024m -cp <path_to_weka.jar> weka.classifiers.rules.PART -C 0.25 -M 2 -no-cv -t <.arff_file_for_trining_the_model> -d <name_for_the_model>'), intern = TRUE)
-
-#command used for Validation and Application. Each separate validation and application set (per monkey and per tissue and time point was individually entered in -T)
-validation_or_application_dataset = system(paste('java --add-opens java.base/java.lang=ALL-UNNAMED -Xmx1024m -cp <path_to_weka.jar> weka.classifiers.rules.PART -l <name_given_to_the_model> -T <.arff_file_for_testing_the_model>'), intern = TRUE)
